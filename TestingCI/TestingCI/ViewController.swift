5967199ec6346a7a0e6730e731f3592719c31672
Francisco Soares <fmssn@cin.ufpe.br>
2017-08-23 03:56:39 -0300

    Initial code commit

---
+++ TestingCI/TestingCI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingCI
+//
+//  Created by Francisco Soares on 23/08/17.
+//  Copyright © 2017 Francisco Soares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
